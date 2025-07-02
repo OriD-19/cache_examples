32d6ed4bb26fc8f76b8fa1f6aff55ffc7b5a9536
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-07-01 18:38:38 -0600

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheService } from './cache.service';
+
+@Module({
+    imports: [TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT!, 10) || 5432,
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'password',
+        database: process.env.DB_NAME || 'test',
+        entities: [],
+        synchronize: true, // Set to false in production
+    })],
+    controllers: [AppController],
+    providers: [AppService, CacheService],
+})
+export class AppModule { }
